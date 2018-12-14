c9cf17126ed059b70e31762ccd5d1c0e92dd4c8b
Stefan Marr <git@stefan-marr.de>
2018-12-14 01:07:05 +0000

    Remove most old repository code
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/repository.ts
+++ src/repository.ts
@@ -7,41 +7,4 @@
  */
 export class Repository {
 
-  public async commitSubmodule() {
-    if (this.updateResult === undefined) {
-      throw new Error("commitSubmodule() used before updateSubmodule().");
-    }
-
-    if (this.updateResult.success === false) {
-      throw new Error("commitSubmodule() should only be used when update was successful.");
-    }
-
-    const prevDate = this.updateResult.heads.beforeUpdate.date;
-    const upstreamDate = this.updateResult.heads.upstream.date;
-    return this.createSubmoduleCommit(prevDate, upstreamDate, this.updateResult.fastForward);
-  }
-
-  protected async createSubmoduleCommit(prevDate: string, upstreamDate: string, fastForward: boolean) {
-    if (!this.subRepo) {
-      throw new Error("commitSubmodule() used before updateSubmodule().");
-    }
-
-    const name = this.config.submodule.path;
-
-    let msg = `Update submodule ${name}
-
-Previous version from: ${prevDate}
-Current version from:  ${upstreamDate}
-
-Updated based on: ${this.config.submodule.upstream.url}
-Using branch:     ${this.config.submodule.upstream.branch}`;
-
-    if (fastForward) {
-      msg += "\n\nThis update was a simple fast-forward.";
-    } else {
-      msg += "\n\nThis update was a rebase without conflicts.";
-    }
-
-    return this.repo.commit(this.config.submodule.path, msg);
-  }
 }
