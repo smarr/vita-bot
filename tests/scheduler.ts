2e8728ee64a6bd5a4aa64bf1ee357e6e8b478c4a
Stefan Marr <git@stefan-marr.de>
2018-12-24 14:22:18 +0100

    Remove unnecessary details from scheduler tests
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- tests/scheduler.ts
+++ tests/scheduler.ts
@@ -33,7 +33,6 @@ const installDetails = [
 ];
 
 function createInstallations() {
-
   const installs: any[] = [];
 
   for (const d of installDetails) {
@@ -42,35 +41,11 @@ function createInstallations() {
       "account": {
         "login": d.accountLogin,
         "id": d.accountId,
-        "node_id": "MDEyOk9yZ2FuaXphdGlvbjE=",
-        "url": GITHUB_API + "/orgs/github",
-        "repos_url": GITHUB_API + "/orgs/github/repos",
-        "events_url": GITHUB_API + "/orgs/github/events",
-        "hooks_url": GITHUB_API + "/orgs/github/hooks",
-        "issues_url": GITHUB_API + "/orgs/github/issues",
-        "members_url": GITHUB_API + "/orgs/github/members{/member}",
-        "public_members_url": GITHUB_API + "/orgs/github/public_members{/member}",
-        "avatar_url": GITHUB_API + "/images/error/octocat_happy.gif",
-        "description": "A great organization"
+        "url": GITHUB_API + "/orgs/github"
       },
-      "access_tokens_url": GITHUB_API + "/installations/" + d.installId + "/access_tokens",
-      "repositories_url": GITHUB_API + "/installation/repositories",
-      "html_url": GITHUB_API + "/organizations/github/settings/installations/" + d.installId,
       "app_id": d.appId,
       "target_id": d.appId,
-      "target_type": "Organization",
-      "permissions": {
-        "metadata": "read",
-        "contents": "read",
-        "issues": "write",
-        "single_file": "write"
-      },
-      "events": [
-        "push",
-        "pull_request"
-      ],
-      "single_file_name": "config.yml",
-      "repository_selection": "selected"
+      "target_type": "Organization"
     });
   }
 
@@ -90,108 +65,16 @@ function createUserRepos(uri: string) {
     const fullName = instDetail.accountLogin + "/" + repo.name;
     repoDetails.push({
       "id": 1296269,
-      "node_id": "MDEwOlJlcG9zaXRvcnkxMjk2MjY5",
       "name": repo.name,
       "full_name": fullName,
       "owner": {
         "login": instDetail.accountLogin,
         "id": instDetail.accountId,
-        "node_id": "MDQ6VXNlcjE=",
-        "avatar_url": "https://github.com/images/error/octocat_happy.gif",
-        "gravatar_id": "",
-        "url": GITHUB_API + "/users/" + instDetail.accountLogin,
-        "html_url": "https://github.com/" + instDetail.accountLogin,
-        "followers_url": GITHUB_API + "/users/" + instDetail.accountLogin + "/followers",
-        "following_url": GITHUB_API + "/users/" + instDetail.accountLogin + "/following{/other_user}",
-        "gists_url": GITHUB_API + "/users/" + instDetail.accountLogin + "/gists{/gist_id}",
-        "starred_url": GITHUB_API + "/users/" + instDetail.accountLogin + "/starred{/owner}{/repo}",
-        "subscriptions_url": GITHUB_API + "/users/" + instDetail.accountLogin + "/subscriptions",
-        "organizations_url": GITHUB_API + "/users/" + instDetail.accountLogin + "/orgs",
-        "repos_url": GITHUB_API + "/users/" + instDetail.accountLogin + "/repos",
-        "events_url": GITHUB_API + "/users/" + instDetail.accountLogin + "/events{/privacy}",
-        "received_events_url": GITHUB_API + "/users/" + instDetail.accountLogin + "/received_events",
         "type": "User",
         "site_admin": false
       },
       "private": false,
-      "html_url": "https://github.com/" + fullName,
-      "description": "This your first repo!",
-      "fork": true,
-      "url": GITHUB_API + "/repos/" + instDetail.accountLogin + "/" + repo.name,
-      "archive_url": GITHUB_API + "/repos/" + fullName + "/{archive_format}{/ref}",
-      "assignees_url": GITHUB_API + "/repos/" + fullName + "/assignees{/user}",
-      "blobs_url": GITHUB_API + "/repos/" + fullName + "/git/blobs{/sha}",
-      "branches_url": GITHUB_API + "/repos/" + fullName + "/branches{/branch}",
-      "collaborators_url": GITHUB_API + "/repos/" + fullName + "/collaborators{/collaborator}",
-      "comments_url": GITHUB_API + "/repos/" + fullName + "/comments{/number}",
-      "commits_url": GITHUB_API + "/repos/" + fullName + "/commits{/sha}",
-      "compare_url": GITHUB_API + "/repos/" + fullName + "/compare/{base}...{head}",
-      "contents_url": GITHUB_API + "/repos/" + fullName + "/contents/{+path}",
-      "contributors_url": GITHUB_API + "/repos/" + fullName + "/contributors",
-      "deployments_url": GITHUB_API + "/repos/" + fullName + "/deployments",
-      "downloads_url": GITHUB_API + "/repos/" + fullName + "/downloads",
-      "events_url": GITHUB_API + "/repos/" + fullName + "/events",
-      "forks_url": GITHUB_API + "/repos/" + fullName + "/forks",
-      "git_commits_url": GITHUB_API + "/repos/" + fullName + "/git/commits{/sha}",
-      "git_refs_url": GITHUB_API + "/repos/" + fullName + "/git/refs{/sha}",
-      "git_tags_url": GITHUB_API + "/repos/" + fullName + "/git/tags{/sha}",
-      "git_url": "git:github.com/" + fullName + ".git",
-      "issue_comment_url": GITHUB_API + "/repos/" + fullName + "/issues/comments{/number}",
-      "issue_events_url": GITHUB_API + "/repos/" + fullName + "/issues/events{/number}",
-      "issues_url": GITHUB_API + "/repos/" + fullName + "/issues{/number}",
-      "keys_url": GITHUB_API + "/repos/" + fullName + "/keys{/key_id}",
-      "labels_url": GITHUB_API + "/repos/" + fullName + "/labels{/name}",
-      "languages_url": GITHUB_API + "/repos/" + fullName + "/languages",
-      "merges_url": GITHUB_API + "/repos/" + fullName + "/merges",
-      "milestones_url": GITHUB_API + "/repos/" + fullName + "/milestones{/number}",
-      "notifications_url": GITHUB_API + "/repos/" + fullName + "/notifications{?since,all,participating}",
-      "pulls_url": GITHUB_API + "/repos/" + fullName + "/pulls{/number}",
-      "releases_url": GITHUB_API + "/repos/" + fullName + "/releases{/id}",
-      "ssh_url": "git@github.com:" + fullName + ".git",
-      "stargazers_url": GITHUB_API + "/repos/" + fullName + "/stargazers",
-      "statuses_url": GITHUB_API + "/repos/" + fullName + "/statuses/{sha}",
-      "subscribers_url": GITHUB_API + "/repos/" + fullName + "/subscribers",
-      "subscription_url": GITHUB_API + "/repos/" + fullName + "/subscription",
-      "tags_url": GITHUB_API + "/repos/" + fullName + "/tags",
-      "teams_url": GITHUB_API + "/repos/" + fullName + "/teams",
-      "trees_url": GITHUB_API + "/repos/" + fullName + "/git/trees{/sha}",
-      "clone_url": "https://github.com/" + fullName + ".git",
-      "mirror_url": "git:git.example.com/" + fullName + "",
-      "hooks_url": GITHUB_API + "/repos/" + fullName + "/hooks",
-      "svn_url": "https://svn.github.com/" + fullName + "",
-      "homepage": "https://github.com",
-      "language": null,
-      "forks_count": 9,
-      "stargazers_count": 80,
-      "watchers_count": 80,
-      "size": 108,
-      "default_branch": "master",
-      "open_issues_count": 0,
-      "topics": [
-        "octocat",
-        "atom",
-        "electron",
-        "API"
-      ],
-      "has_issues": true,
-      "has_projects": true,
-      "has_wiki": true,
-      "has_pages": false,
-      "has_downloads": true,
-      "archived": false,
-      "pushed_at": "2011-01-26T19:06:43Z",
-      "created_at": "2011-01-26T19:01:12Z",
-      "updated_at": "2011-01-26T19:14:43Z",
-      "permissions": {
-        "admin": false,
-        "push": false,
-        "pull": true
-      },
-      "allow_rebase_merge": true,
-      "allow_squash_merge": true,
-      "allow_merge_commit": true,
-      "subscribers_count": 42,
-      "network_count": 0
+      "fork": true
     });
   }
 
@@ -201,12 +84,12 @@ function createUserRepos(uri: string) {
   };
 }
 
-const appInstallations = nock(GITHUB_API)
+nock(GITHUB_API)
   .get("/app/installations")
   .times(3)
   .reply(200, createInstallations());
 
-const accessToken = nock(GITHUB_API)
+nock(GITHUB_API)
   .post(/\/app\/installations\/\d+\/access_tokens/)
   .times(3 * 3)
   .reply(201, {
@@ -214,8 +97,7 @@ const accessToken = nock(GITHUB_API)
     "expires_at": "2016-07-11T22:14:10Z"
   });
 
-
-const userInstallationsRepos = nock(GITHUB_API)
+nock(GITHUB_API)
   .get(userInstallRegex)
   .times(10 * 3)
   .reply(200, createUserRepos);
